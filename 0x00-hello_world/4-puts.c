b3e86677cc10538419b0cc80f0110e08573450fb
Ani Chidera <aniwinner70@gmail.com>
2022-10-13 18:04:11 -0700

    4puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Entry point for arguement
+ * Return: zero if code is correct
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
