0132335f1d5af2b97cbb057c381b8d0d9087d80d
Ani Chidera <aniwinner70@gmail.com>
2022-10-13 17:16:54 -0700

    lets print f

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * I'm trying to create a printf code
+ * my code should return zero if there are no errors
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return 0; 
+}
