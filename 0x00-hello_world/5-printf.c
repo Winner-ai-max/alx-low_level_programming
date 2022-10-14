e5661ddf2d20dde8da444f2de8e056a817617dbf
Ani Chidera <aniwinner70@gmail.com>
2022-10-13 17:24:18 -0700

    printing f

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * I'm trying to create a printf code
- * my code should return zero if there are no errors
+ * main -entry
+ * Return -always print 0
  */
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return 0; 
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
