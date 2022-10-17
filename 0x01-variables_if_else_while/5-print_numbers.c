672d661604020fa5e3842de9489b6878bec6d5b2
Ani Chidera <aniwinner70@gmail.com>
2022-10-17 16:39:37 -0700

    single digit 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main-entry point for project
+ *
+ * Return:0 if code runs without error
+ */
+int main(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		putchar (c);
+	}
+	putchar ('\n');
+	return (0);
+}
