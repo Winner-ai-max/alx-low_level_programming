33c37a150658fb191dc2dc9dcafc12e5f4633e4d
Ani Chidera <aniwinner70@gmail.com>
2022-10-17 10:19:39 -0700

    lower case from a new line

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main- Entry point to program
+ * Return:0 if cofe rurns without error
+ */
+int main(void)
+{
+	char c;
+	{
+
+	for (c = 'a'; c <= 'z'; c++)
+		if (c != 'e' && c != 'q')
+			continue;
+				putchar (c);
+	putchar ('\n');
+	}
+	return (0);
+}
