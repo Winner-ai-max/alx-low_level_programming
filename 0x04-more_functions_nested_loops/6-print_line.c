81f82760a9ddb1468c07a977643ac27b589259d1
Ani Chidera <aniwinner70@gmail.com>
2022-10-21 05:25:25 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_line - print a straight line using '_'
+ * @n: The new number of '_' to be printed
+ * Return: 0
+ */
+void print_line(int n)
+{
+	print_line(0);
+	print_line(2);
+	print_line(10);
+	print_line(-4);
+	return (0);
+}
